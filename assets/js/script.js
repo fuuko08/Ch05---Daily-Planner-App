5c19797435c715221906836510054932ee5bac22
Selina Su <phongsu20@gmail.com>
2022-09-18 18:15:47 -0700

    I think i finish the app

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,36 @@
 var currentDay = moment();
 $("#currentDay").text(currentDay.format("MMM Do, YYYY"));
 
-// Click on Save Button
+// Save input data
 let saveBtn = $('.saveBtn');
 let inputNote = "";
 let inputTime = "";
 
 saveBtn.on('click', function(event) {
     event.preventDefault();
-    inputTime = parseInt($(this).parent().attr("id"));
+    inputTime = $(this).attr("id");
     inputNote = $(this).siblings(".description").val();
-    console.log(inputTime);
-    console.log(inputNote);
+    alert("Your task has been saved in local storage!")
     saveInput();
+    
 })
+
+function saveInput() {
+    localStorage.setItem(inputTime, inputNote);
+}
+
+// Get input data
+var timeBlockArr = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+
+function getData() {
+    for (let i = 9; i < 17; i++) {
+        $("#timeblock-[i]").each(function (i, v) {
+            $(v).val(localStorage.getItem(timeBlockArr[i]));
+        })
+    }
+};
+getData();
+
 
 // Function reset hour
 var interval = setInterval(updateTime, 60000);
@@ -23,6 +40,7 @@ var interval = setInterval(updateTime, 60000);
 
 var timeBlock = Array.from(document.querySelectorAll(".time-block"));
 console.log(timeBlock);
+
 function updateTime() {
     var currentHour = moment().hours();
     console.log(currentHour);
@@ -40,22 +58,3 @@ function updateTime() {
         } console.log(blockHour);
     }
 updateTime(); 
-
-// Save input data
-function saveInput() {
-    localStorage.setItem(inputTime, inputNote);
-}
-
-// Get input data
-var timeBlockArr = [9, 10, 11, 12, 13, 14, 15, 16, 17];
-
-function getInput() {
-    for ( var i = 0; i < timeBlockArr.length; i++) {
-        $("[id^=timeblock-]").each(function (i, v) {
-            $(v).val(localStorage.getItem(timeBlockArr[i]));
-        }) 
-    }
-    console.log($(v).val);
-};
-getInput();
-
